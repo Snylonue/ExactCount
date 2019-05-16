cc35bf0223c9b2fbf88857fe5209ee3222c75cd3
Snylonue <48825598+Snylonue@users.noreply.github.com>
2019-05-16 21:27:57 +0800

    Update ExactSqrt.py
    加入分解质因数函数 f_temp()  
    加入临时质数判断函数 check_prime()

--- ExactSqrt.py
+++ ExactSqrt.py
@@ -1,3 +1,4 @@
+import numpy
 def include(l,num):
 	if (l==[]):
 		return False
@@ -22,9 +23,30 @@ def add_sqrt(l):
 			s.append([t[0],t[2]])
 		#s=
 	return s
+def check_prime(num):
+	t=2
+	max=int(numpy.sqrt(num))
+	if (num<=0 or num==1 or num%2==0):
+		return False
+	elif (num==3):
+		return True
+	else:
+		while (t<=max):
+			if (num%t==0):
+				return False
+			elif (t==max):
+				return True
+			else:
+				t+=1
+def f_temp(num):
+	l=[]
+	l.append(num)
+	while (check_prime(l[-1])==False):
+		for x in range(2,l[-1]):
+			if (l[-1]%x==0):
+				l.append(x)
+				l[-1],l[-2]=int(l[-2]/l[-1]),l[-1]
+				break
+	return l
 def simple(l):
-	#
-a=['6_sqrt_15','8_sqrt_15']
-sum=add_sqrt(a)
-
-print(sum)
+	pass
