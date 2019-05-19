99a0124e5216c161617c91c0401d2d52292b2e6d
Snylonue <snylonue@gmail.com>
2019-05-19 13:55:55 +0800

    Update ExactSqrt.py
    重写 add_sqrt()，check_prime()函数
    f_temp()改名为prime_factor()
    将include()和point()函数合并为inpoint()

--- ExactSqrt.py
+++ ExactSqrt.py
@@ -1,47 +1,25 @@
 import numpy
-def include(l,num):
-	if (l==[]):
-		return False
-	for x in range(len(l)-1):
-		if (l[x]==num):
-			return True
-		elif (x==len(l)-1):
-			return False
-def point(l,num):
-	for x in range(len(l)-1):
-		if (l[x][1]==num):
-			return x
-def add_sqrt(l):
-	s=[]
-	for x in range(1,len(l)):
-		t=l[x].split('_')
-		t[0],t[2]=int(t[0]),int(t[2])
-		if (include(s,t[2])):
-			p=point(s,t[2])
-			s[p][0]+=t[0]
-		else:
-			s.append([t[0],t[2]])
-		#s=
-	return s
 def check_prime(num):
-	t=2
-	max=int(numpy.sqrt(num))
-	if (num<=0 or num==1 or num%2==0):
+	if (num==1):
 		return False
-	elif (num==3):
+	if (num==2):
 		return True
-	else:
-		while (t<=max):
-			if (num%t==0):
-				return False
-			elif (t==max):
-				return True
-			else:
-				t+=1
-def f_temp(num):
+	max=int(numpy.sqrt(num))+1
+	for x in range(3,max,2):
+		if (num%x==0):
+			return False
+		elif (x==max-1):
+			return True
+def inpoint(l,num):
+	for v,x in enumerate(l):
+		if (x[2]==num):
+			return True,v
+		elif (v==len(l)-1):
+			return False,0
+def prime_factor(num):
 	l=[]
 	l.append(num)
-	while (check_prime(l[-1])==False):
+	while (check_prime(l[-1])==False and l[-1]!=1):
 		for x in range(2,l[-1]):
 			if (l[-1]%x==0):
 				l.append(x)
@@ -50,3 +28,20 @@ def f_temp(num):
 	return l
 def simple(l):
 	pass
+def add_sqrt(l):
+	sum=[]
+	for v,x in enumerate(l):
+		x[0],x[2]=int(x[0]),int(x[2])
+		if (v!=0):
+			include,point=inpoint(sum,x[2])	
+			if (include):
+				sum[point][0]+=x[0]
+			else:
+				sum.append([x[0],x[1],x[2]])
+		else:
+			sum.append([x[0],x[1],x[2]])
+	return sum
+a=['6_sqrt_15','8_sqrt_15']
+for x in range(len(a)):
+	a[x]=a[x].split('_')
+print(add_sqrt(a))
