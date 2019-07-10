5f48e71a477711476ed71e92fac4ff3795e65cfe
Snylonue <snylonue@gmail.com>
2019-07-10 13:30:15 +0800

    使用python自带分数模块

--- ExactCount.py
+++ ExactCount.py
@@ -2,9 +2,10 @@
 
 from math import gcd
 from numpy import sqrt
+from fractions import Fraction
 
 class Number_tools(object):
-	def notPrime(self,num):
+	def __notPrime(self,num):
 		if (type(num)!=int):
 			return True
 		elif (num<=1 or num%2==0 or num%3==0 or num%5==0 or num%7==0):
@@ -18,7 +19,7 @@ class Number_tools(object):
 	def isPrime(self,num):
 		if (num in [2,3,5,7,11,13,17,19]):
 			return True
-		elif (self.notPrime(num)):
+		elif (self.__notPrime(num)):
 			return False
 		else:
 			max=int(numpy.sqrt(num))+1
@@ -36,23 +37,15 @@ class Number_tools(object):
 					l[-1]//=x
 					break
 		return l
-	def lcm(self,l):
+	def lcm(self,l):	#can be deleted
 		slcm=lambda x,y:x//gcd(x,y)*y
 		while (len(l)>1):
 			t=(slcm(l[0],l[1]))
 			l.pop(0)
 			l[0]=t
 		return l[0]
-class Frac(Number_tools):
-	def __init__(self,mole,deno):
-		self.mole=mole
-		self.deno=deno
-	def simple(self):
-		m=gcd(self.mole,self.deno)
-		self.mole//=m
-		self.deno//=m
 class Root(Number_tools):
-	def __init__(self,modu,base):
+	def __init__(self,modu=Fraction(),base=1):
 		self.modu=modu
 		self.base=base
 	def simple(self):
@@ -60,71 +53,40 @@ class Root(Number_tools):
 			pass
 		else:
 			m=number(self.factor(self.base))
-			t=Frac(m,1)
-			fp=Frac_processing()
-			self.modu=fp.multiply_frac([t,self.modu])
-			self.modu.simple()
+			self.modu*=m
 			self.base//=m**2
-class Frac_processing(Number_tools):
-	def add_frac(self,l):
-		t,r=set([]),[0,0]
-		for x in l:
-			t.add(x.deno)
-		m=self.lcm(list(t))
-		for x in l:
-			r[0]+=m//x.deno*x.mole
-			r[1]+=m
-		re=Frac(r[0],r[1])
-		re.simple()
-		return re
-	def multiply_frac(self,l):
-		s0,s1=1,1
-		for x in l:
-			s0*=x.mole
-			s1*=x.deno
-		re=Frac(s0,s1)
-		re.simple()
-		return re
-	def devide_frac(self,l):
-		for x,v in enumerate(l):
-			if (x==0):
-				continue
-			else:
-				v.mole,v.deno=v.deno,v.mole
-		return self.multiply_frac(l)
-class Root_processing(Frac_processing):
-	def add_root(self,l):
+	def frac_to_int(self):
+		self.modu/=self.base.denominator
+		self.base=self.base.numerator*self.base.denominator
+class Root_processing(Number_tools):
+	def add(self,l):
 		s,re={},[]
 		for x in l:
 			x.simple()
 			if (x.base in s):
-				s[x.base]=self.add_frac([s[x.base],x.modu])
+				s[x.base]+=x.modu
 			else:
 				s[x.base]=x.modu
 		for x,v in s.items():
 			re.append(Root(v,x))
 		return re
-	def multiply_root(self,l):
-		modus,bases=[],1
+	def multiply(self,l):
+		res=Root(Fraction(1,1),1)
 		for x in l:
-			modus.append(x.modu)
-			bases*=x.base
-		res_modus=self.devide_frac(modus)
-		res=Root(res_modus,bases)
+			res.modu*=x.modu
+			res.base*=x.base
 		res.simple()
 		return res
-	def devide_root(self,l):
-		modus,s_temp=[],1
-		for x in l:                #处理系数
-			modus.append(x.modu)
-		res_modus=self.devide_frac(modus)
+	def devide(self,l):
+		res=l[0]
 		for x,v in enumerate(l):
 			if (x==0):
 				continue
 			else:
-				s_temp*=v.base
-		res=Root(Frac(1,s_temp),l[0].base*s_temp)
-		res.modu=self.multiply_frac([res.modu,res_modus])
+				res.modu/=v.modu
+				res.base/=v.base
+		if (type(res.base)==Fraction):
+			res.frac_to_int()
 		res.simple()
 		return res
 
@@ -147,13 +109,11 @@ def count(d):
 		s*=x**v
 	return s
 def input_format(l):
-	re=[]
+	res=[]
 	for x in l:
-		if (type(x[1])==list):
-			t=gcd(x[1][0],x[1][1])
-			x[1][0]//=t
-			x[1][1]//=t
-			x[0][1]*=x[1][1]
-			x[1]=x[1][0]*x[1][1]
-		re.append(Root(Frac(x[0][0],x[0][1]),x[1]))
-	return re
+		x=Root(Fraction(x[0][0],x[0][1]),x[1])
+		if (type(x.base)==list):
+			x.base=Fraction(x.base[0],x.base[1])
+			x.frac_to_int()
+		res.append(x)
+	return res
